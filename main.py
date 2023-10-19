e39607e575121dcea215f56a10c54a30d7b12c4b
“YelyzavetaFedenko” <yelyzaveta.fedenko@hebron-academy.com>
2023-10-19 14:53:41 +0300

    Solution

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ registered_users = ["user1", "user2", "user3"]
 
 
 # Декоратор для перевірки, чи користувач зареєстрований
-
+def user_registered(func):
+    def wrapper(username, data):
+        if username in registered_users:
+            return func(username, data)
+        else:
+            return "Wrong credentials, please try again."
+    return wrapper
 
 
 # Приклад використання декоратора
